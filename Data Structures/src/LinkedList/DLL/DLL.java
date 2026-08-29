49136896f59cc690af7f2ff0d77066deadbb86fe
RevanthkumarSathiasilane <revanthkumarsathiasilane2908@gmail.com>
2026-08-29 11:31:39 +0530

    Linked List Basics finsihed

--- Data Structures/src/LinkedList/DLL/DLL.java
+++ Data Structures/src/LinkedList/DLL/DLL.java
@@ -1,5 +1,7 @@
 package LinkedList.DLL;
 
+import javax.crypto.spec.PSource;
+
 public class DLL {
     Node head;
     Node tail;
@@ -12,15 +14,58 @@ public class DLL {
         int value;
         Node next;
         Node prev;
-
         public Node(int value) {
             this.value = value;
         }
-
         public Node(int value, Node next, Node prev) {
             this.value = value;
             this.next = next;
             this.prev = prev;
         }
     }
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next=head;
+        node.prev = null;
+        if(head!=null){
+            head.prev = node;
+        }
+        head = node;
+    }
+    public void revDisplay(){
+        Node temp = head;
+        Node last = null;
+        while(temp!=null){
+            last = temp;
+            temp = temp.next;
+        }
+        System.out.print("END -> ");
+        while(last!=null){
+            System.out.print(last.value+" -> ");
+            last = last.prev;
+        }
+    }
+    public void insertLast(int val){
+        if(head==null){
+            insertFirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        Node last = head;
+        while(last.next!=null){
+            last = last.next;
+        }
+        node.prev = last;
+        node.next = null;
+        last.next = node;
+
+    }
+   public void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.value +" -> ");
+            temp = temp.next;
+        }
+       System.out.println("END");
+   }
 }
--- Data Structures/src/LinkedList/DLL/Main.java
+++ Data Structures/src/LinkedList/DLL/Main.java
@@ -3,5 +3,11 @@ package LinkedList.DLL;
 public class Main {
     public static void main(String[] args) {
         DLL dll = new DLL();
+        dll.insertFirst(1);
+        dll.insertFirst(2);
+        dll.insertFirst(3);
+        dll.insertLast(0);
+        dll.display();
+       // dll.revDisplay();
     }
 }
