7fe6a6bc01e8b08fcec443da2bf4c50aef80341f
RevanthkumarSathiasilane <revanthkumarsathiasilane2908@gmail.com>
2026-08-30 14:13:07 +0530

    Length of a Linked List Cycle finsihed

--- Data Structures/src/LinkedList/Questions/LengthOfALinkedListCycle.java
+++ Data Structures/src/LinkedList/Questions/LengthOfALinkedListCycle.java
@@ -17,7 +17,49 @@ public class LengthOfALinkedListCycle {
         }
         return dummy.next;
     }
+    static void display(Node head){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.value+" -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+    static void convertTocycle(Node head,int pos){
+        int index = 0;
+        Node current = head;
+        Node cycleNode = null;
+        while(current.next!=null){
+            if(pos == index){
+                cycleNode = current;
+            }
+            current = current.next;
+            index+=1;
+        }
+        current.next = cycleNode;
+    }
+    static int lengthOfCycle(Node head){
+        int length = 0;
+        Node fast = head;
+        Node slow = head;
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                Node temp = slow;
+                do{
+                    length+=1;
+                    temp = temp.next;
+                }while(temp!=slow);
+                return length;
+            }
+        }
+        return length;
+    }
     public static void main(String[] args) {
-
+         Node head = create(new int[]{1,2,3,4,5,6,7,8});
+         display(head);
+         convertTocycle(head,3);
+        System.out.println("Length of Cyclic Node : "+ lengthOfCycle(head));
     }
 }
