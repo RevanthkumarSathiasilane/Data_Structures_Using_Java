0c3b54fd311731da5c558fad82931a0381ec8fc4
RevanthkumarSathiasilane <revanthkumarsathiasilane2908@gmail.com>
2026-09-03 20:14:12 +0530

    Started Stack and Queue questions

---
+++ Data Structures/src/StackAndQueue/Questions/ImplementQueueUsingStacks.java
@@ -0,0 +1,9 @@
+package StackAndQueue.Questions;
+import java.util.*;
+public class ImplementQueueUsingStacks {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(5);
+        System.out.println(stack.pop());
+    }
+}
