8c6119b0767fce7d14ba153a04ec1e29a5bcfd5b
RevanthkumarSathiasilane <revanthkumarsathiasilane2908@gmail.com>
2026-09-03 20:49:34 +0530

    Implemented Queue using Stacks (HINT : TWO STACKS)

--- Data Structures/src/StackAndQueue/Questions/ImplementQueueUsingStacks.java
+++ Data Structures/src/StackAndQueue/Questions/ImplementQueueUsingStacks.java
@@ -2,8 +2,58 @@ package StackAndQueue.Questions;
 import java.util.*;
 public class ImplementQueueUsingStacks {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(5);
-        System.out.println(stack.pop());
+
+    }
+}
+class QueueUsingStack{
+    private Stack<Integer> first;
+    private Stack<Integer> second;
+
+    public QueueUsingStack(){
+        first = new Stack<>();
+        second = new Stack<>();
+    }
+    public int remove() throws Exception{
+        return first.pop();
+    }
+    public void insert(int item){
+        while(!first.isEmpty()){
+            second.push(first.pop());
+        }
+        first.push(item);
+        while(!second.isEmpty()){
+            first.push(second.pop());
+        }
+    }
+    /*
+    Insertion = O(1);
+    Removal = O(n);
+    public void insert(int item){
+        first.push(item);
+    }
+    public int remove() throws Exception{
+        while(!first.isEmpty()) {
+            second.push(first.pop());
+        }
+        int removed = second.pop();
+        while(!second.isEmpty()){
+            first.push(second.pop());
+        }
+        return removed;
+    }
+    */
+
+    public int peek() throws Exception{
+        while(!first.isEmpty()){
+            second.push(first.pop());
+        }
+        int peak = second.peek();
+        while(!second.isEmpty()){
+            first.push(second.pop());
+        }
+        return peak;
+    }
+    public boolean isEmpty(){
+        return first.isEmpty();
     }
 }
