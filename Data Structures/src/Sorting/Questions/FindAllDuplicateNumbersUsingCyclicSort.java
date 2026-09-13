11fa3c8a237fc8a1ac16bfa39e96235a6b0a8ad5
RevanthkumarSathiasilane <revanthkumarsathiasilane2908@gmail.com>
2026-09-13 08:28:50 +0530

    Finded all duplicate numbers from an array using cyclic Sort

--- Data Structures/src/Sorting/Questions/FindAllDuplicateNumbersUsingCyclicSort.java
+++ Data Structures/src/Sorting/Questions/FindAllDuplicateNumbersUsingCyclicSort.java
@@ -1,26 +1,24 @@
 package Sorting.Questions;
 import java.util.*;
-public class FindAllDisappearedNumbersInArrayUsingCyclicSort {
+public class FindAllDuplicateNumbersUsingCyclicSort {
     public static void main(String[] args) {
-        int[] arr = {1,1,2};//{4,3,2,7,8,2,3,1};
-        ArrayList<Integer> list = cyclicSort(arr);
-        System.out.println(list);
+       int[] arr = {4,3,2,7,8,2,3,1};
+        System.out.println(cyclicSort(arr));
     }
-    static ArrayList<Integer> cyclicSort(int[] arr){
-        ArrayList<Integer> list  = new ArrayList<>();
+    static List<Integer> cyclicSort(int[] arr){
         int i = 0;
         while(i<arr.length){
             int correct = arr[i]-1;
             if(arr[i]!=arr[correct]){
                 swap(arr,i,correct);
-            }
-            else{
+            }else{
                 i++;
             }
         }
-        for(int c = 0;c<arr.length;c++){
-             if(arr[c]-1 != c){
-                list.add(c+1);
+        List<Integer> list = new ArrayList<>();
+        for(int c=0;c<arr.length;c++){
+            if(arr[c]-1 != c){
+                list.add(arr[c]);
             }
         }
         return list;
